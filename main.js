146a42a4e60bc9c1ea480b91b7ac200edd7e3fa3
Skyfe24 <fabio.erase@gmail.com>
2023-05-28 16:51:05 +0200

    first result

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('JS OK');
 // il numero è sempre tra 0 e 0.99999//
-const random = Math.random();
-console.log(random);
+const random = 0.999999;
+console.log('random number:' + random);
 
 //proviamo con una variabile massima ad esempio 5//
 
@@ -9,6 +9,6 @@ const max= 5;
 
 // così non supererà mai il 5 /
 let result = Math.floor(random * max);
-console.log (result);
+console.log ('result:' result);
 
-//aggiungiamo Math.floor per ELIMINARE LA PARTE DECIMALE
+//aggiungiamo Math.floor prima della parentesi per ELIMINARE LA PARTE DECIMALE
