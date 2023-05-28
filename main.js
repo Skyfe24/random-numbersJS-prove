f1440361707b01545494fb3acb5e6450e7e718b6
Skyfe24 <fabio.erase@gmail.com>
2023-05-28 17:06:05 +0200

    third result (vogliamo random number ma minimo 1 max 5 incluso)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('JS OK');
 // il numero è sempre tra 0 e 0.99999//
-const random = 0.999999;
+const random = Math.random();
 console.log('random number:' + random);
 
 // 1) proviamo con una variabile massima ad esempio 5 MA NON VOGLIAMO ESCA 5//
@@ -14,5 +14,9 @@ let result = Math.floor(random * max);
 //aggiungiamo Math.floor prima della parentesi per ELIMINARE LA PARTE DECIMALE
 
 // 2) proviamo con una variabile massima ad esempio 5 //
- result = Math.floor(random * max + 1); //aggiungiamo + 1 così andrà ad escludere 1 unità in più in questo caso 6//
+ result = Math.floor(random * max)+ 1; //aggiungiamo + 1 così andrà ad escludere 1 unità in più in questo caso 6//
+//console.log ('result:' + result);//
+
+// 2) proviamo con una variabile massima ad esempio 5 ma vogliamo partire MINIMO DA 1 //
+result = Math.floor(random * max) + 1; //aggiungiamo + 1 così andrà ad escludere 1 unità in più in questo caso 6//
 console.log ('result:' + result);
